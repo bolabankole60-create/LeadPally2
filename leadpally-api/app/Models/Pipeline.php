d6cb41f65a001dac41cd9ab8387c908d45b3c904
bolabankole60-create <bolabankole60@gmail.com>
2026-07-01 12:44:31 +0100

    Add real pipeline model

---
+++ leadpally-api/app/Models/Pipeline.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pipeline extends Model
+{
+    protected $fillable = [
+        'team_id',
+        'name',
+        'is_default',
+    ];
+
+    protected $casts = [
+        'is_default' => 'boolean',
+    ];
+
+    public function stages()
+    {
+        return $this->hasMany(PipelineStage::class)->orderBy('position');
+    }
+
+    public function deals()
+    {
+        return $this->hasMany(Deal::class);
+    }
+}
